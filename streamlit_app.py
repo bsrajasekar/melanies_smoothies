8a67798d9f0aa5d9d4160ebdf3f6d7721f4a58fb
bsrajasekar <bs_rajasekar@yahoo.co.in>
2025-11-04 05:34:51 +0000

    Added a lookup column for searching specific fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,10 @@ session = cnx.session()
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name who ordered Smoothie is:', name_on_order)
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data = my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data = my_dataframe, use_container_width=True)
+st.stop()
+
 ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
     #st.write(ingredients_list)
@@ -30,6 +32,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
